60d27f20eb10933c710bfd90fef45b0d5aca0f3c
Jason van Gumster <jason@monsterjavaguns.com>
2016-11-05 00:16:38 -0400

    First real commit.
    These are just the base sox commands as noted from honkeymagoo in the
    LinuxLUGcast show notes. There's a lot of room for improvement here.

---
+++ mumble2pdocast.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# A relatively simple script for taking a mumble recording and converting it to a podcast
+#
+# Requires that you have sox installed.
+#
+# Usage: $ mumble2podcast.sh <input_file.wav>
+
+sourceaudio=$(basename "$1")
+introaudio="intro.wav" #XXX Assumes intro.wav is in the local directory
+outroaudio="outro.wav" #XXX Assumes outro.wav is in the local directory
+
+echo "Truncating silence from $1 to $sourceaudio.truncated.wav"
+
+sox -S $1 $sourceaudio.truncated.wav silence 1 0.1 0% -1 0.1 0%
+
+echo "Homogenizing sample rates to 48000 Hz"
+
+sox $sourceaudio.truncated.wav -r 48000 $sourceaudio.48kHz.wav
+
+echo "Adding Intro and Outro"
+
+sox intro.wav $sourceaudio.48kHz.wav outro.wav podcast.ogg #XXX Would be nice to specify the output at the command line
